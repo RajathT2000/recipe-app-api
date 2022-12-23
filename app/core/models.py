b6371ad287cf332481845edc90c9c736f2a87334
Rajath <rajath@qburst.com>
2022-12-23 15:17:48 +0530

    Made some change to linting-1

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,19 @@
-from django.db import models  # noqa
-
-# Create your models here.
-from django.contrib.auth.models import AbstractBaseUser, \
-    BaseUserManager, PermissionsMixin
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
 
 class UserManager(BaseUserManager):
+    """Manager for users."""
+
     def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
         if not email:
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -16,6 +23,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
+        """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -25,10 +33,12 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
+    
     USERNAME_FIELD = 'email'
